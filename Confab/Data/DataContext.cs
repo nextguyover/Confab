5f7121e6c6ab9b005f221c9a872b28047433f910
nextguyover <56286401+nextguyover@users.noreply.github.com>
2024-08-13 17:19:42 +1000

    removed unused code

--- Confab/Data/DataContext.cs
+++ Confab/Data/DataContext.cs
@@ -5,9 +5,7 @@ namespace Confab.Data
 {
     public class DataContext : DbContext
     {
-        public DataContext(DbContextOptions<DataContext> options) : base(options) {
-            
-        }
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
 
         public DbSet<UserSchema> Users => Set<UserSchema>();
         public DbSet<CommentSchema> Comments => Set<CommentSchema>();
@@ -15,31 +13,5 @@ namespace Confab.Data
         public DbSet<GlobalSettingsSchema> GlobalSettings => Set<GlobalSettingsSchema>();
         public DbSet<CommentLocationSchema> CommentLocations => Set<CommentLocationSchema>();
         public DbSet<AutoModerationRuleSchema> AutoModerationRules => Set<AutoModerationRuleSchema>();
-        //public DbSet<VoteSchema> Votes => Set<VoteSchema>();
-
-        //protected override void OnModelCreating(ModelBuilder modelBuilder)
-        //{
-        //    modelBuilder.Entity<CommentSchema>()
-        //        .HasOne(c => c.Author)
-        //        .WithMany(u => u.AuthoredComments)
-        //        .HasForeignKey(c => c.AuthorId);
-
-        //    // Configure other relationships as needed
-
-        //    base.OnModelCreating(modelBuilder);
-        //}
-
-        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        //{
-        //    optionsBuilder
-        //        .UseSqlServer("your_connection_string")
-        //        .UseLazyLoadingProxies() // Enable lazy loading proxies
-        //        .ConfigureWarnings(warnings => warnings.Ignore(CoreEventId.DetachedLazyLoadingWarning));
-
-        //    optionsBuilder.UseJsonOptions(jsonOptions =>
-        //    {
-        //        jsonOptions.SerializerOptions.ReferenceHandler = ReferenceHandler.Preserve;
-        //    });
-        //}
     }
 }
